3a8fdb219250884375df451c70e3cecf8169c5cb
Vergill <vergillrl@gmail.com>
2018-09-15 15:01:51 +0200

    Added labels for 2nd signature and username

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,6 +1,4 @@
-/* global chrome, version, getText, sourceUrl, sourceUrl2, riseRegex, leaveAnimation */
-let activeSource
-
+/* global chrome, version, getText, riseRegex, leaveAnimation */
 /**
  * Read all necessary info from localStorage and populate the web page with that information whenever the options page is opened
  */
@@ -18,21 +16,11 @@ function start () {
     'watchmessages',
     'lastsystemblockheight',
     'riseusd',
-    'risebtc',
-    'useSource',
-    'source3'
+    'risebtc'
   ], function (item) {
     let numberOfValidAddresses = ([ item.address1, item.address2, item.address3, item.address4, item.address5 ].filter(c => c.match(riseRegex))).length
     const watchmessages = item.watchmessages.toString()
     document.getElementById('numberofvalidaddresses').textContent = numberOfValidAddresses === 1 ? numberOfValidAddresses.toString() + ' RISE ' + getText('address') : numberOfValidAddresses.toString() + ' RISE ' + getText('addresses')
-    // sourceUrl and sourceUrl2 are defined in functions.js; only the user defined source3 is retrieved from local storage
-    if (item.useSource.toString() === '1') {
-      activeSource = sourceUrl
-    } else if (item.useSource.toString() === '2') {
-      activeSource = sourceUrl2
-    } else if (item.useSource.toString() === '3') {
-      activeSource = item.source3
-    }
     if (watchmessages === '3') {
       document.getElementById('watchingmessages').textContent = getText('watch_outgoing')
     } else if (watchmessages === '2') {
@@ -40,51 +28,14 @@ function start () {
     } else {
       document.getElementById('watchingmessages').textContent = getText('watch_all')
     }
-    if (!getPrice()) {
-      // if getPrice fails then use the last recorded values from storage
-      document.getElementById('riseusd').textContent = item.riseusd.toString()
-      document.getElementById('risebtc').textContent = item.risebtc.toString()
-    }
+    // runs the function checkPrice from background.js to get the latest price info from data source
+    chrome.runtime.getBackgroundPage((background) => {
+      background.checkPrice(false, (resp) => {
+        document.getElementById('riseusd').textContent = item.riseusd.toString()
+        document.getElementById('risebtc').textContent = item.risebtc.toString()
+      })
+    })
   })
-}
-
-/**
- * Request price info from source
- */
-function getPrice () {
-  if (activeSource !== undefined) {
-    const sourcePrice = activeSource.endsWith('/') ? activeSource + 'prices/' : activeSource + '/prices/'
-    let xhr = new window.XMLHttpRequest()
-    xhr.open('GET', sourcePrice, true)
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          try {
-            const response = JSON.parse(xhr.responseText)
-            if (Array.isArray(response)) {
-              const resp = response[0]
-              if (typeof resp === 'object' && resp.id.toString().toUpperCase() === 'RISE') {
-                chrome.storage.local.set({ riseusd: resp.price_usd, risebtc: resp.price_btc }, () => {
-                  document.getElementById('riseusd').textContent = resp.price_usd.toString()
-                  document.getElementById('risebtc').textContent = resp.price_btc.toString()
-                  return true
-                })
-              }
-            }
-          } catch (e) {
-            console.warn(`Could not get price from ${sourcePrice}:\n`, e)
-            return false
-          }
-        } else {
-          console.warn(`Could not get price from ${sourcePrice}`)
-          return false
-        }
-      }
-    }
-    xhr.send()
-  } else {
-    return false
-  }
 }
 
 /**
